ea907e0c973ff06ba2b062e479c82b6cb8b68503
James <justjamescreative@outlook.com>
2025-02-04 11:40:27 -0500

    Updated the StudentSortingApplication
    Cleared the project for a restart

---
+++ src/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+
+    private final int studentID;
+    private final String studentName;
+    private final String course;
+    private final int grade;
+
+    public Student(int studentID, String studentName, String course, int grade) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString() {
+        return studentID + "," + studentName + "," + course + "," + grade + "\n";
+    }
+}
--- src/StudentSortingApplication.java
+++ src/StudentSortingApplication.java
@@ -1,17 +1,4 @@
-import com.assignment4.models.Student;
-import com.assignment4.services.ReadFileService;
-import com.assignment4.services.WriteFileService;
-
-import java.util.List;
-
 public class StudentSortingApplication {
 
-    public static void main(String[] args) {
-
-        ReadFileService readFileService = new ReadFileService();
-        WriteFileService writeFileService = new WriteFileService();
-
-        List<Student> lines = readFileService.readFile("student-master-list.csv");
-//        writeFileService.writeFile("course.csv", lines);
-    }
+    public static void main(String[] args) {}
 }
