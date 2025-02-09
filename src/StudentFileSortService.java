0efa3350ae5305f6d8133d6574bb3b8c5e14471b
James <justjamescreative@outlook.com>
2025-02-09 14:37:19 -0500

    Removed the FileSortService and updated StudentFileSortService
    Cleaned up the codebase

--- src/StudentFileSortService.java
+++ src/StudentFileSortService.java
@@ -3,9 +3,7 @@ import java.util.List;
 
 public class StudentFileSortService {
 
-    private final FileSortService fileSortService = new FileSortService();
-
     public void sortStudentsByDescendingGrade(List<Student> students) {
-        fileSortService.sort(students, Comparator.nullsLast(Comparator.comparing(Student::getGrade).reversed()));
+        students.sort(Comparator.nullsLast(Comparator.comparing(Student::getGrade).reversed()));
     }
 }
